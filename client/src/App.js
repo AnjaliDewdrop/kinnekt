d9555cd48624501e759ccbe9cf567f5714cc4849
Annie Xiao <alxiao@edu.uwaterloo.ca>
2019-03-03 09:02:52 -0500

    UI updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import circuitPhoto from './photos/circuit-decorations.png'
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,6 +24,10 @@ class App extends Component {
       <div className="app">
         <header className="kinnekt-header">
           <h1 className="app-title">Kinnekt</h1>
+          <p>
+            This webapp aims to help you learn about circuits
+            without having to get the hardware yourself.
+          </p>
         </header>
         <div className='centered-container'>
           <div className='side-bar'>
@@ -40,9 +46,10 @@ class App extends Component {
           <div className='exercise-wrapper'>
             <div className="exercise">
               {console.log(this.state.test)}
-              {!(this.state.test) ? (
-                <p>Got here</p>
-              ) : <p>{this.state.test}</p>}
+              {/* {!(this.state.test) ? (
+                <img id='circuit-photo' src={circuitPhoto}/>
+              ) : <p>{this.state.test}</p>} */}
+              <img id='circuit-photo' src={circuitPhoto}/>
             </div>
             <div className='button-wrapper'>
               <button id='submit-button' onClick={this.handleButtonClick} >
