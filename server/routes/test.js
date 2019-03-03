1be0174c0b158074653e89fe60035c31fbc13536
Annie Xiao <alxiao@edu.uwaterloo.ca>
2019-03-03 10:46:55 -0500

    Unify with master

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
-/* GET test */
-router.get('/', function(req, res, next) {
-=======
 const util = require('util');
 const fs = require('fs');
 const TrainingApi = require("azure-cognitiveservices-customvision-training");
@@ -12,32 +8,47 @@ const PredictionApi = require("azure-cognitiveservices-customvision-prediction")
 
 const setTimeoutPromise = util.promisify(setTimeout);
 
-const trainingKey = "7176670c7aa14a749414cc93894c1211";
-const predictionKey = "3bc0bc8e42eb428ab9a2d0aff467674c";
-const sampleDataRoot = __dirname + "/voltDivider";
+const trainingKey = "bcc5c88e3d9e45f3b9676ca34321aa09";
+const predictionKey = "d46476deaab6464380f96cd9fbabb210";
+const sampleDataRoot = "./voltDivider";
 
 const endPoint = "https://southcentralus.api.cognitive.microsoft.com"
 
 const trainer = new TrainingApi(trainingKey, endPoint);
 
-async function kinnekt() {
+async function apiCall() {
     console.log("Creating project...");
-    const sampleProject = await trainer.createProject("Sample Project")
+    const sampleProject = await trainer.createProject("Sample Project");
     const hemlockTag = await trainer.createTag(sampleProject.id, "Hemlock");
-    const cherryTag = await trainer.createTag(sampleProject.id, "Japanese Cherry");
+    const cherryTag = await trainer.createTag(
+        sampleProject.id,
+        "Japanese Cherry"
+    );
     console.log("Adding images...");
     let fileUploadPromises = [];
-
+    
     const hemlockDir = `${sampleDataRoot}/Hemlock`;
     const hemlockFiles = fs.readdirSync(hemlockDir);
     hemlockFiles.forEach(file => {
-        fileUploadPromises.push(trainer.createImagesFromData(sampleProject.id, fs.readFileSync(`${hemlockDir}/${file}`), { tagIds: [hemlockTag.id] }));
+        fileUploadPromises.push(
+        trainer.createImagesFromData(
+            sampleProject.id,
+            fs.readFileSync(`${hemlockDir}/${file}`),
+            { tagIds: [hemlockTag.id] }
+        )
+        );
     });
 
     const cherryDir = `${sampleDataRoot}/Japanese Cherry`;
     const japaneseCherryFiles = fs.readdirSync(cherryDir);
     japaneseCherryFiles.forEach(file => {
-        fileUploadPromises.push(trainer.createImagesFromData(sampleProject.id, fs.readFileSync(`${cherryDir}/${file}`), { tagIds: [cherryTag.id] }));
+        fileUploadPromises.push(
+        trainer.createImagesFromData(
+            sampleProject.id,
+            fs.readFileSync(`${cherryDir}/${file}`),
+            { tagIds: [cherryTag.id] }
+        )
+        );
     });
 
     await Promise.all(fileUploadPromises);
@@ -49,31 +60,47 @@ async function kinnekt() {
     while (trainingIteration.status == "Training") {
         console.log("Training status: " + trainingIteration.status);
         await setTimeoutPromise(1000, null);
-        trainingIteration = await trainer.getIteration(sampleProject.id, trainingIteration.id)
+        trainingIteration = await trainer.getIteration(
+        sampleProject.id,
+        trainingIteration.id
+        );
     }
     console.log("Training status: " + trainingIteration.status);
 
     // Update iteration to be default
     trainingIteration.isDefault = true;
-    await trainer.updateIteration(sampleProject.id, trainingIteration.id, trainingIteration);
+    await trainer.updateIteration(
+        sampleProject.id,
+        trainingIteration.id,
+        trainingIteration
+    );
     const predictor = new PredictionApi(predictionKey, endPoint);
     const testFile = fs.readFileSync(`${sampleDataRoot}/Test/test1.png`);
 
-    const results = await predictor.predictImage(sampleProject.id, testFile, { iterationId: trainingIteration.id });
+    const results = await predictor.predictImage(sampleProject.id, testFile, {
+        iterationId: trainingIteration.id
+    });
 
     // Step 6. Show results
     console.log("Results:");
     results.predictions.forEach(predictedResult => {
-        console.log(`\t ${predictedResult.tagName}: ${(predictedResult.probability * 100.0).toFixed(2)}%`);
+        console.log(
+        `\t ${predictedResult.tagName}: ${(
+            predictedResult.probability * 100.0
+        ).toFixed(2)}%`
+        );
     });
-    console.log('Done.')
-};
+}
 
 /* GET test */
 router.get('/', function(req, res, next) {
-    kinnekt()
->>>>>>> 7299d57c6818a9996e176908c6b2eaa617f7a47b
-    res.send({ message: 'HI!!!' })
+    var number = Math.floor(Math.random() * Math.floor(max));
+    apiCall()
+    if (number % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 });
 
 module.exports = router;
